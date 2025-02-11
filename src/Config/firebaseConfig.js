6b04ae7affae45a325f7987ab50556d0378b77d4
Anisul Islam <anis.inbox10@gmail.com>
2025-02-11 23:07:37 +0500

    hide keys

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUIJrwoOa9i8mkSzkKoadXX7ot97zSnJc",
-  authDomain: "brainbay007.firebaseapp.com",
-  projectId: "brainbay007",
-  storageBucket: "brainbay007.appspot.com",
-  messagingSenderId: "1010641016845",
-  appId: "1:1010641016845:web:5808799dd5155525c45d44",
-  measurementId: "G-Q4R9KEWGY7"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app); // Export Firestore
+export const db = getFirestore(app);
