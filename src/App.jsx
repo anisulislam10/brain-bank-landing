949f0dac416acb51e509859950876e50d98c90ec
Anisul Islam <anis.inbox10@gmail.com>
2025-02-14 19:12:48 +0500

    router file move to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { HelmetProvider } from "react-helmet-async";
 import React from "react";
+import { Routes, Route } from "react-router-dom"; // ✅ Import Routes & Route
 import './App.css';
-import Home from './page/Home';
 import 'animate.css';
+import Home from './page/Home';
+import SignupForm from "./page/Signup.jsx";
+import ScheduleDemoForm from "./page/ContactForm.jsx";
+import Investor from "./page/Investor.jsx";
 
 function App() {
   return (
     <HelmetProvider>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/contactform" element={<ScheduleDemoForm />} />
+        <Route path="/investor" element={<Investor />} />
+      </Routes>
     </HelmetProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // ✅ Import Routes and Route
+import { BrowserRouter } from "react-router-dom"; // Keep BrowserRouter only
 import App from "./App.jsx";
-import SignupForm from "./page/Signup.jsx";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import ScheduleDemoForm from "./page/ContactForm.jsx";
-import ProblemSolutionSection from "./components/OurSolution.jsx";
-import Investor from "./page/Investor.jsx";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/signup" element={<SignupForm />} />
-      <Route path="/contactform" element={<ScheduleDemoForm />} />
-      <Route path="/investor" element={<Investor />} />
-
-
-    </Routes>
+    <App />
   </BrowserRouter>
 );
